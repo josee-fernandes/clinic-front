5fc149c297a066219706f69acdcb0d3d58a6e59c
José Vitor <jose-vitor-7@hotmail.com>
2025-02-25 23:07:19 -0300

    chore: update eslint settings to be compatible with v9

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
-import { NextPage } from "next";
-
+import { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
-    <div />
-  );
+    <div className="border bg-red-500 text-white">
+      <h1>Welcome to the Home Page</h1>
+      <p>This is a simple Next.js application.</p>
+    </div>
+  )
 }
 
 Home.displayName = 'Home'
