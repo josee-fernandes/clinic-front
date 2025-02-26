eadb37ca68c61e5fbfe8dbcab884f2d1190b81e3
José Vitor <jose-vitor-7@hotmail.com>
2025-02-26 00:12:20 -0300

    force push

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,7 @@
 import { NextPage } from 'next'
 
 const Home: NextPage = () => {
-  return (
-    <div className="border bg-red-500 text-white">
-      <h1>Welcome to the Home Page</h1>
-      <p>This is a simple Next.js application.</p>
-    </div>
-  )
+  return <div>Olá!</div>
 }
 
 Home.displayName = 'Home'
